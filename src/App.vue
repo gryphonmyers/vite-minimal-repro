9cd767ea6e939a319cfbd3324bc39ab3a86b437a
Gryphon Myers <gryphon.myers@2k.com>
2021-01-22 11:23:28 +0800

    add additional context

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,8 @@ export default {
   }
 }
 </script>
+<style>
+  .main-component {
+    font-weight: bold;
+  }
+</style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="main-component">This should be red text</div>
+    <div class="main-component">This text should be red and bold</div>
 </template>
 <style>
     .main-component {
