88e6b5c43f6ffd4d69e0e27e9248748f111f318e
Gryphon Myers <gryphon.myers@2k.com>
2021-01-25 19:15:41 +0800

    minimal repro for ws issue

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import vue from '@vitejs/plugin-vue'
  * @type {import('vite').UserConfig}
  */
 export default {
-  build: {
-    cssCodeSplit: false
-  },
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    hmr: {
+      path: '/foobar'
+    }
+  }
 }
